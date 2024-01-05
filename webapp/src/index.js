992fb010a956ecc47edf081888d281ca5c257eae
jagmohan <jagmohan.singh.1@gmail.com>
2024-01-05 13:03:01 +0530

    render events on calendar

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -10,83 +10,34 @@ import 'select2/dist/css/select2.css';
 import './index.css';
 
 document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('calendarx');
-    $('.combo').select2({width: 'resolve'});
-  
-    var calendar = new Calendar(calendarEl, {
-      plugins: [ interactionPlugin, dayGridPlugin, timeGridPlugin, listPlugin ],
-      headerToolbar: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
-      },
-      initialDate: '2018-01-12',
-      navLinks: true, // can click day/week names to navigate views
-      editable: true,
-      dayMaxEvents: true, // allow "more" link when too many events
-      events: [
-        {
-          title: 'All Day Event',
-          start: '2018-01-01',
-        },
-        {
-          title: 'Long Event',
-          start: '2018-01-07',
-          end: '2018-01-10'
-        },
-        {
-          groupId: 999,
-          title: 'Repeating Event',
-          start: '2018-01-09T16:00:00'
-        },
-        {
-          groupId: 999,
-          title: 'Repeating Event',
-          start: '2018-01-16T16:00:00'
-        },
-        {
-          title: 'Conference',
-          start: '2018-01-11',
-          end: '2018-01-13'
-        },
-        {
-          title: 'Meeting',
-          start: '2018-01-12T10:30:00',
-          end: '2018-01-12T12:30:00'
-        },
-        {
-          title: 'Lunch',
-          start: '2018-01-12T12:00:00'
-        },
-        {
-          title: 'Meeting',
-          start: '2018-01-12T14:30:00'
-        },
-        {
-          title: 'Happy Hour',
-          start: '2018-01-12T17:30:00'
-        },
-        {
-          title: 'Dinner',
-          start: '2018-01-12T20:00:00'
-        },
-        {
-          title: 'Birthday Party',
-          start: '2018-01-13T07:00:00'
-        },
-        {
-          title: 'Click for Google',
-          url: 'http://google.com/',
-          start: '2018-01-28'
-        }
-      ]
-    });
-  
-    calendar.render();
+  // initiate full calendar
+  window.calendarX = initFullCalendar();
+  // initiate select fields
+  $('.combo').select2({width: 'resolve'});
+  // get calendar entries
+  getCalendarEntries(null, null);
 });
 
 window.filterData = function() {
   getCalendarEntries($('#parentCampaign').val(), $('select.campaignType').val());
+}
+
+function initFullCalendar() {
+  var calendarEl = document.getElementById('calendarx');
+  var cal = new Calendar(calendarEl, {
+    plugins: [ interactionPlugin, dayGridPlugin, timeGridPlugin, listPlugin ],
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+    },
+    initialDate: new Date(),
+    navLinks: true, // can click day/week names to navigate views
+    editable: true,
+    dayMaxEvents: true, // allow "more" link when too many events
+  });
+  cal.render();
+  return cal;
 }
 
 function getCalendarEntries(parentCampaignId, status){
@@ -97,6 +48,9 @@ function getCalendarEntries(parentCampaignId, status){
   if (status == null) {
     status = '';
   }
+
+  // remove existing events
+  window.calendarX.removeAllEvents();
   
   Visualforce.remoting.Manager.invokeAction(                    
     RemoteAction.getCalendarEntry,
@@ -107,7 +61,8 @@ function getCalendarEntries(parentCampaignId, status){
           this.start = parseStartDate(this.startDate);
           this.end = parseEndDate(this.endDate);
         });
-        console.log(result);
+        window.calendarX.addEventSource(result);
+        window.calendarX.render();
       } else {
         alert(event.message);
       }
@@ -117,9 +72,9 @@ function getCalendarEntries(parentCampaignId, status){
 }
 
 function parseStartDate(data){
-  return moment.utc(data);
+  return moment.utc(data).format('YYYY-MM-DD');
 }
 
 function parseEndDate(data){
-  return moment.utc(data + 86400000);
+  return moment.utc(data + 86400000).format('YYYY-MM-DD');
 }
